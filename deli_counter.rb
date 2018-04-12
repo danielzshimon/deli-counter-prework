882934bba0a4bc2b173a98ba85b07d0f1e24fa89
Daniel Shimon <danielzshimon@gmail.com>
2018-04-12 16:37:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,4 +16,6 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
   
-def
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts 
